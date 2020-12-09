492b80cdfc73a5f3409854bf8a4d6911a69ea303
cmerino01 <merinobr@usc.edu>
2020-12-09 14:28:37 -0800

    Update earth.js

--- earth.js
+++ earth.js
@@ -4,7 +4,7 @@ function initialize() {
     var marker = WE.marker([51.5, -0.09]).addTo(earth);
     marker.bindPopup("<b>Hello world!</b><br>I am a popup.<br /><span style='font-size:10px;color:#999'>Tip: Another popup is hidden in Cairo..</span>", {maxWidth: 150, closeButton: true}).openPopup();
     var marker2 = WE.marker([30.058056, 31.228889]).addTo(earth);
-    marker2.bindPopup("<b>Cairo</b><br>Yay, you found me!", {maxWidth: 120, closeButton: true}).openPopup();
+    marker2.bindPopup("<b>Pablo</b><br>You're stupid!", {maxWidth: 120, closeButton: true}).openPopup();
 
     
     earth.setView([20, -105], 2.5);
