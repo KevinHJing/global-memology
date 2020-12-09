371e51164ccc9060fa06e83ad9ab06326ef4cc84
Pablo Tayun-Mazariegos <tayunmaz@usc.edu>
2020-12-09 13:43:08 -0800

    fixed close button

--- earth.js
+++ earth.js
@@ -2,12 +2,10 @@ function initialize() {
     var earth = new WE.map('earth_div');
     WE.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(earth);
     var marker = WE.marker([51.5, -0.09]).addTo(earth);
-    marker.bindPopup("<b>Hello world!</b><br>I am a popup.<br /><span style='font-size:10px;color:#999'>Tip: Another popup is hidden in Cairo..</span>", {maxWidth: 150, closeButton: false});
+    marker.bindPopup("<b>Hello world!</b><br>I am a popup.<br /><span style='font-size:10px;color:#999'>Tip: Another popup is hidden in Cairo..</span>", {maxWidth: 150, closeButton: true}).openPopup();
     var marker2 = WE.marker([30.058056, 31.228889]).addTo(earth);
-    marker2.bindPopup("<b>Cairo</b><br>Yay, you found me!", {maxWidth: 120, closeButton: false});
-    console.log(marker.bindPopup);
+    marker2.bindPopup("<b>Cairo</b><br>Yay, you found me!", {maxWidth: 120, closeButton: true}).openPopup();
 
-    var markerCustom = WE.marker([50, -9], '/img/logo-webglearth-white-100.png', 100, 24).addTo(earth);
     
     earth.setView([51.505, 0], 6);
     
