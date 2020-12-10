21f60cab923ff8ebdbfe2e5b7049c4dd532001a3
Pablo Tayun-Mazariegos <tayunmaz@usc.edu>
2020-12-09 21:16:07 -0800

    popups appear only when zoomed in

--- earth.js
+++ earth.js
@@ -3,16 +3,18 @@ function initialize() {
     var earth = new WE.map('earth_div');
     WE.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(earth);
 
-    if(earth.getZoom() > 5 )
+    
+    /*if(Math.round(earth.getZoom()) > 5 )
     {
         var marker = WE.marker([51.5, -0.09]).addTo(earth);
         marker.bindPopup("<b>Hello world!</b><br>I am a popup.<br /><span style='font-size:10px;color:#999'>Tip: Another popup is hidden in Cairo..</span>", {maxWidth: 150, closeButton: true}).openPopup();
         var marker2 = WE.marker([30.058056, 31.228889]).addTo(earth);
         marker2.bindPopup("<b>Pablo</b><br>You're stupid!", {maxWidth: 120, closeButton: true}).openPopup();
+        
     }
+    */
     
     
-    earth.setView([20, -105], 2.5);
     
 
     
@@ -22,10 +24,13 @@ function initialize() {
             var c = earth.getPosition();
             var elapsed = before? now - before: 0;
             before = now;
-            
             if(Math.round(earth.getZoom()) >= 5)
             {
                 earth.setCenter([c[0], c[1]]);
+                var marker = WE.marker([51.5, -0.09]).addTo(earth);
+                marker.bindPopup("<b>Hello world!</b><br>I am a popup.<br /><span style='font-size:10px;color:#999'>Tip: Another popup is hidden in Cairo..</span>", {maxWidth: 150, closeButton: true}).openPopup();
+                var marker2 = WE.marker([30.058056, 31.228889]).addTo(earth);
+                marker2.bindPopup("<b>Pablo</b><br>You're stupid!", {maxWidth: 120, closeButton: true}).openPopup();
             }
             else
             {
