92f60f218024651e2a8fb7e64ac9154fb4f3d190
Pablo Tayun-Mazariegos <tayunmaz@usc.edu>
2020-12-09 20:24:03 -0800

    both can now rotate

--- earth.js
+++ earth.js
@@ -34,5 +34,6 @@ function initialize() {
             }
             requestAnimationFrame(animate);
         });
+    }
 }
 
--- moon.js
+++ moon.js
@@ -3,12 +3,12 @@ function initializeMoon() {
     var moon = new WE.map('moon_div');
     WE.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(moon);
     
-    /*
+    
     var marker = WE.marker([51.5, -0.09]).addTo(moon);
     marker.bindPopup("<b>Hello world!</b><br>I am a popup.<br /><span style='font-size:10px;color:#999'>Tip: Another popup is hidden in Cairo..</span>", {maxWidth: 150, closeButton: true}).openPopup();
     var marker2 = WE.marker([30.058056, 31.228889]).addTo(moon);
     marker2.bindPopup("<b>Pablo</b><br>You're stupid!", {maxWidth: 120, closeButton: true}).openPopup();
-    */
+    
 
     moon.setView([20, -105], 2.5);
     
