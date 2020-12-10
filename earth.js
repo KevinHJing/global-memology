fa9b5e2aae5bf7484a7e01e97c65986b7c223cc0
cmerino01 <merinobr@usc.edu>
2020-12-09 21:55:44 -0800

    Update earth.js

--- earth.js
+++ earth.js
@@ -28,15 +28,15 @@ function initialize() {
             {
                 earth.setCenter([c[0], c[1]]);
 
-                var marker3 = WE.marker([10, 10]).addTo(earth);
-                marker3.bindPopup("<img src='assets/Memes/Germany/c86a1e3837abd60c5a070efc166c8cbb.jpg'>", {closeButton: true}).openPopup();
+                //var marker3 = WE.marker([10, 10]).addTo(earth);
+                //marker3.bindPopup("<img src='assets/Memes/Germany/c86a1e3837abd60c5a070efc166c8cbb.jpg'>", {closeButton: true}).openPopup();
 
                 if(Math.round(earth.getZoom()) >= 5)
                 {
                     var marker = WE.marker([51.5, -0.09]).addTo(earth);
                     marker.bindPopup("<b>Hello world!</b><br>I am a popup.<br /><span style='font-size:10px;color:#999'>Tip: Another popup is hidden in Cairo..</span>", {maxWidth: 150, closeButton: true}).openPopup();
                     var marker2 = WE.marker([30.058056, 31.228889]).addTo(earth);
-                    marker2.bindPopup("<b>Pablo</b><br>You're stupid!", {maxWidth: 120, closeButton: true}).openPopup();
+                    marker2.bindPopup("<img src='assets/Memes/Germany/c86a1e3837abd60c5a070efc166c8cbb.jpg'>", {maxWidth: 500, closeButton: true}).openPopup();
                     earth.setZoom(4);
                 }
                 
