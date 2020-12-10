ae3903b19492d2acdbd700bf4cc809a20ec693bc
cmerino01 <merinobr@usc.edu>
2020-12-10 09:56:04 -0800

    Update earth.js

--- earth.js
+++ earth.js
@@ -5,21 +5,21 @@ function initialize() {
 
     // initialize markers and pop ups
     var marker = WE.marker([51.5, -0.09]);
-    marker.bindPopup("<img src='assets/Memes/UK/uk_image.jpg'>", {maxWidth: 250, closeButton: true}).openPopup(); //UK
+    marker.bindPopup("<img src='assets/Memes/UK/uk_image.jpg'>", {maxWidth: 250, closeButton: false}).openPopup(); //UK
     var marker2 = WE.marker([51.5, 10]);
-    marker2.bindPopup("<img src='assets/Memes/Germany/germany_image.jpg'>", {maxWidth: 250, closeButton: true}).openPopup(); //Germany
+    marker2.bindPopup("<img src='assets/Memes/Germany/germany_image.jpg'>", {maxWidth: 250, closeButton: false}).openPopup(); //Germany
     var marker3 = WE.marker([35, 135]);
-    marker3.bindPopup("<img src='assets/Memes/Japan/japan_image.jpg'>", {maxWidth: 250, closeButton: true}).openPopup(); //Japan
+    marker3.bindPopup("<img src='assets/Memes/Japan/japan_image.jpg'>", {maxWidth: 250, closeButton: false}).openPopup(); //Japan
     var marker4 = WE.marker([24, 255]);
-    marker4.bindPopup("<img src='assets/Memes/Mexico/mx_image.jpg'>", {maxWidth: 250, closeButton: true}).openPopup(); //Mexico
+    marker4.bindPopup("<img src='assets/Memes/Mexico/mx_image.jpg'>", {maxWidth: 250, closeButton: false}).openPopup(); //Mexico
     var marker5 = WE.marker([40, -8]);
-    marker5.bindPopup("<img src='assets/Memes/Portugal/portugal_image.jpg'>", {maxWidth: 250, closeButton: true}).openPopup();//Portugal
+    marker5.bindPopup("<img src='assets/Memes/Portugal/portugal_image.jpg'>", {maxWidth: 250, closeButton: false}).openPopup();//Portugal
     var marker6 = WE.marker([60, 17]);
-    marker6.bindPopup("<img src='assets/Memes/Sweden/sweden_image.jpg'>", {maxWidth: 250, closeButton: true}).openPopup();//Sweden
+    marker6.bindPopup("<img src='assets/Memes/Sweden/sweden_image.jpg'>", {maxWidth: 250, closeButton: false}).openPopup();//Sweden
     var marker7 = WE.marker([15, 105]);
-    marker7.bindPopup("<img src='assets/Memes/Vietnam/vietnam_image.jpg'>", {maxWidth: 250, closeButton: true}).openPopup(); //Vietnam
+    marker7.bindPopup("<img src='assets/Memes/Vietnam/vietnam_image.jpg'>", {maxWidth: 250, closeButton: false}).openPopup(); //Vietnam
     var marker8 = WE.marker([34, 242]);
-    marker8.bindPopup("<img src='assets/Memes/US/USA_image.jpg'>", {maxWidth: 250, closeButton: true}).openPopup(); //USA
+    marker8.bindPopup("<img src='assets/Memes/US/USA_image.jpg'>", {maxWidth: 250, closeButton: false}).openPopup(); //USA
 
     // array of markers
     var markerArr = [marker, marker2, marker3, marker4, marker5, marker6, marker7, marker8];
